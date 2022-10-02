8b77b51097b2065225a6fefd55bcedd600b3a5b5
Sejin Lee <lsj8706@naver.com>
2022-10-02 22:16:51 +0900

    [Setting] #1 - 프로젝트 생성

---
+++ SOPT-31st-Assignment/SOPT-31st-Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOPT-31st-Assignment
+//
+//  Created by sejin on 2022/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
