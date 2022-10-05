cc5d0d6673c30a51591d6cd20b21952daf6cc698
Sejin Lee <lsj8706@naver.com>
2022-10-05 11:06:58 +0900

    [Feat] #3 - UserModel 생성

---
+++ SOPT-31st-Assignment/SOPT-31st-Assignment/Domain/Model/UserModel.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  SOPT-31st-Assignment
+//
+//  Created by sejin on 2022/10/05.
+//
+
+import Foundation
+
+struct UserModel {
+    var emailOrPhoneNumber: String
+    var password: String
+}
